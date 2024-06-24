355a379ff77ec996abb0205afc5435a741b6932e
unknown <khushiparekh1507@gmail.com>
2024-06-24 17:38:59 +1200

    setup multer

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import express from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./user/userRouter";
 import createBook from "./controllers/createBook";
+import registerController from "./controllers/registerController";
+import cors from "cors";
+import { config } from "./config/config";
 const app =express();
 //Routes......
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -9,8 +12,15 @@ app.get('/',(req,res,next)=>{
     res.json({messge:'Welcome'});
 
 });
+app.use(
+    cors({
+        origin:config.frontendDomain,
+    })
+)
 app.use("/api/users",userRouter);
 app.use("/api/books",createBook);
+app.use('api/users/register',registerController);
+
 app.use(globalErrorHandler);
 
 export default app;
